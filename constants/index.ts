17dbb5e876806dda2230df71a69c8edf77c6b62d
Fernando <fer.oliveeira@hotmail.com>
2024-01-03 04:14:09 -0300

    Navbar component created

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'projects', label: 'Projetos' },
+    { href: '/', key: 'socials', label: 'Redes Socias' },
+  ];
